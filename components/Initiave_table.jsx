5a6a4c4c9f4372fbeeb51b2cf5a62dcd584bce6e
Pedro Figueira <zepedro0901@gmail.com>
2025-04-28 22:30:18 +0100

    messed around with components and tested the styles

---
+++ components/Initiave_table.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles/InitTable.styles.css"
+
+const InitTable = () => {
+  return ( 
+    <div className="init_table_container">
+     <div style={{border:"solid 1 px black"}}>
+        Init
+     </div>
+    </div>
+  );
+};
+
+export default InitTable
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../components/Sidebar.jsx";
+import InitTable from "../../components/Initiave_table.jsx";
+
 
 export default function Home() {
   return (
     <div> 
-      Hello World!
+      <div className="container" style={{display:"flex",justifyContent:"space-between"}}> 
+        <Sidebar/>
+        <InitTable/>
+        <Sidebar/>
+      </div>
     </div>
    )
   
